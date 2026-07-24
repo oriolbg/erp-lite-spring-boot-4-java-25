6f37e6e45819c9f1757b4027e255dfbfe5f1feda
oriolbg <joborrasg@gmail.com>
2026-07-23 21:44:35 -0300

    Primer commit

---
+++ erp-lite/src/test/java/com/example/erp_lite/ErpLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.erp_lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
